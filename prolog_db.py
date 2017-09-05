b13c11baf561d2cac41da6c2dfa3963e116c0803
mike <mike@lcd-outlet.com>
2017-09-05 20:34:03 +0000

    prolog db

--- prolog_db.py
+++ prolog_db.py
@@ -1,11 +1,11 @@
 from sys import argv
 import re
-f=open('prolog.pl','w')
+f=open('prolog.pl','a+')
 g=open(argv[1])
 data=g.readlines()
 
 for line in data:
-	file_name=re.sub(r'_following|.|/','',argv[1])
+	file_name=re.sub(r'_following|\.|/','',argv[1])
         line = line.replace('\n','')	
 	f.write("following('{}','{}').".format(file_name,line))
 	f.write('\n')
