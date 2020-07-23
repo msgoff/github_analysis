6f00c5db7dd23e2e44c09c21f7a7a68c7a4a83dc
mike <mike@lcd-outlet.com>
2020-07-23 21:44:20 +0000

    unused

--- clean_people_file.sh
+++
@@ -1,1 +0,0 @@
-cat ms_people |grep login|sort |uniq|cut -d ":" -f2|tr -d '"'|tr -d ' ' |tr -d ',' >people
--- run_api.sh
+++
@@ -1,4 +0,0 @@
-cat *following|sort |uniq > ALL
-ls *following | cut -d "_" -f1  > existing
-diff -i ALL existing | grep "<" |tr -d "< " > GET_NEW
-while read f;do bash api_calls.sh $f following 1;bash api_calls.sh $f repos 1;done < <(cat GET_NEW)
