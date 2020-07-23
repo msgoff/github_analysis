f43cfb44ebcf209d56e14b0e305396160e549dca
mike <mike@lcd-outlet.com>
2020-07-23 20:44:06 +0000

    improve readability

--- pull_data.sh
+++ pull_data.sh
@@ -1,35 +1,19 @@
-#set year 
-year=2016
-#you can change 12 to however many months you want out of the year
-for month in {1..12};do
-  
-
-  num_of_days_in_month=$(
-
-	cal -d $year-$month-01 | 
-	          tr " "  '\n'|
-	          grep -P '\d'|
-		  tail -n 1
-		
-			)
-
-echo $num_of_days_in_month
+echo arg1 year
+echo arg2 month
+year="$1"
+month="$2"
+num_of_days_in_month="$(cal -d "$year"-"$month"-01|tr ' ' '\n'|grep  '^[0-9]'|grep -v "$year"|wc -l)"
 
 
-
-=====================================================================================================
-
-#https://www.githubarchive.org/  
-#wget http://data.githubarchive.org/2015-01-{01..30}-{0..23}.json.gz
-
-
-url=http://data.githubarchive.org/$year-0$month-{01..$num_of_days_in_month}-{0..23}.json.gz
-echo $url
-wget $(eval echo $url)
+if [[ $month -lt 10 ]];
+then
+	url=http://data.githubarchive.org/$year-0$month-{01..$num_of_days_in_month}-{0..23}.json.gz
+else
+	url=http://data.githubarchive.org/$year-$month-{01..$num_of_days_in_month}-{0..23}.json.gz
+fi
 
 
-=====================================================================================================
+echo "$(eval echo $url )"|tr ' ' '\n'|xargs -i wget {}
 
 
 
-done
