2e11a3ee081220c4cc841deb7cb7268fac93da23
mike <mike@lcd-outlet.com>
2020-07-23 21:03:24 +0000

    shellcheck

--- pull_data.sh
+++ pull_data.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo arg1 year
 echo arg2 month
 year="$1"
@@ -7,9 +8,9 @@ num_of_days_in_month="$(cal -d "$year"-"$month"-01|tr ' ' '\n'|grep  '^[0-9]'|gr
 
 if [[ $month -lt 10 ]];
 then
-	url=http://data.githubarchive.org/$year-0$month-{01..$num_of_days_in_month}-{0..23}.json.gz
+	url="http://data.githubarchive.org/$year-0$month-{01..$num_of_days_in_month}-{0..23}.json.gz"
 else
-	url=http://data.githubarchive.org/$year-$month-{01..$num_of_days_in_month}-{0..23}.json.gz
+	url="http://data.githubarchive.org/$year-$month-{01..$num_of_days_in_month}-{0..23}.json.gz"
 fi
 
 
